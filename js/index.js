e4a427973b9a29892253fda6742c6260a12d8ca0
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-07-18 20:49:36 -0500

    list books and show details

--- js/index.js
+++ js/index.js
@@ -1,1 +1,46 @@
-document.addEventListener("DOMContentLoaded", function() {});
+document.addEventListener("DOMContentLoaded", ()=>{
+    fetch('http:localhost:3000/books')
+    .then(resp=>resp.json())
+    .then(data=>data.forEach(book=>displayBook(book)))
+});
+
+function displayBook(bookInfo){
+    const bookContainer = document.getElementById('list')
+    const bookTitle = document.createElement('li')
+    bookTitle.id = bookInfo.id
+    bookTitle.innerText = bookInfo.title
+    bookContainer.appendChild(bookTitle)
+    bookTitle.addEventListener('click', e=>showDetails(bookInfo))
+}
+
+function showDetails(bookInfo){
+    const showPanel = document.getElementById('show-panel')
+    showPanel.innerText = ''
+
+    const img = document.createElement('img')
+    img.src = bookInfo.img_url
+    showPanel.appendChild(img)
+
+    const title = document.createElement('h2')
+    title.innerText = bookInfo.title
+    showPanel.appendChild(title)
+
+    const author = document.createElement('h2')
+    author.innerText = bookInfo.author
+    showPanel.appendChild(author)
+
+    const description = document.createElement('p')
+    description.innerText = bookInfo.description
+    showPanel.appendChild(description)
+
+    bookInfo.users.forEach(user => userList(user))
+}
+
+function userList(user){
+    console.log(user.username)
+    const showPanel = document.getElementById('show-panel')
+    const usernameLI = document.createElement('li')
+    usernameLI.innerText = user.username
+
+    showPanel.appendChild(usernameLI)
+}
